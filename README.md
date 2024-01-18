3cf0b615dfeaee646485721e40f6165c3d0a5048
unknown <jero.alderete@hotmail.com>
2024-01-18 16:48:50 -0300

    El Oraculo App

---
+++ README.md
@@ -0,0 +1,16 @@
+# oraculo-de-habitos
+Aplicacion simple en React sobre una tirada de cartas con redireccion automatica a redes sociales y boton de whatsapp para nuestro cliente Oráculo de hábitos.
+
+## Available Scripts
+
+En este proyecto es necesario instalar las dependencias, ejecutar en la terminal de tu editor de codigo:
+### `npm install`
+### `npm start`
+
+Se ejecuta la app en modo local.<br />
+Abrir [http://localhost:3000](http://localhost:3000) para ejecutar la app en el navegador.
+
+<br />
+
+
+
---
+++ src/components/Card.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import ReactCardFlip from 'react-card-flip';
+import backFace from '../images/sub25/oraculo2.jpeg';
+import classnames from 'classnames';
+
+const Card = ({ name, number, frontFace, flipCard, unflippedCards, disabledCards}) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [hasEvent, setHasEvent] = useState(true);
+
+
+  const cardImageClass = classnames('card-image', { '': isFlipped });
+  const cardImageClassFront = classnames('card-image', { 'isflip': isFlipped });
+
+  useEffect(() => {
+    if (unflippedCards.includes(number)) {
+      setTimeout(() => setIsFlipped(false), 700);
+    }
+  }, [unflippedCards])
+
+  useEffect(() => {
+    if (disabledCards.includes(number)) {
+      setHasEvent(false);
+    }
+  }, [disabledCards])
+
+  const handleClick = e => {
+    const value = flipCard(name, number);
+    if (value !== 0) {
+      setIsFlipped(!isFlipped);
+     
+      setTimeout(function() {
+        window.location.href = 'https://www.instagram.com/habitosvsvicios/'; 
+      }, 2500);
+    }
+  }
+
+  let className = 'card-image';
+if (isFlipped) {
+  className += ' isflip';
+}
+
+  return (
+    <div className='card' >
+      <ReactCardFlip className='cards' isFlipped={isFlipped} >
+      <img    
+          className={classnames(cardImageClass, { 'no-event': !hasEvent })}
+          src={backFace}
+          alt='back-face'
+          onClick={hasEvent ? handleClick : null}
+        />
+        <img
+           
+          className={classnames(cardImageClassFront, { 'no-event': !hasEvent })}
+          src={frontFace}
+          alt='front-face'
+          onClick={hasEvent ? handleClick : null}
+        />
+      </ReactCardFlip>
+    </div>
+  )
+}
+
+export default Card
