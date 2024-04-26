af71b49a6ed1ce49d4f6499f0010dd6eee95399d
unknown <jero.alderete@hotmail.com>
2024-04-26 09:47:08 -0300

    asd

---
+++ src/import.js
@@ -0,0 +1,69 @@
+import src1 from './images/sub25/reciclar.jpeg';
+import src2 from './images/sub25/poner-un-limite.png';
+import src3 from './images/sub25/acompanar-a-quien-te-necesite.png';
+import src4 from './images/sub25/descartar-algo-que-no-usas.png';
+import src5 from './images/sub25/aprender-algo-nuevo.png';
+import src6 from './images/sub25/organiza-tu-economia.png';
+import src7 from './images/sub25/evita-compra-necesaria.png';
+import src8 from './images/sub25/agradecer.png';
+import src9 from './images/sub25/tomar-dos-litros-de-agua.png';
+import src10 from './images/sub25/caminar-30.png';
+import src11 from './images/sub25/leer-10-paginas-de-un-libro.png';
+import src12 from './images/sub25/escribir-tus-pensamientos.png';
+
+
+/* BASE DE DATOS LOCAL DE LAS CARTAS DEL ORACULO DE HABITOS */
+
+const images = [
+  {
+    src: src1,
+    player: 'Ansu Fati'
+  },
+  {
+    src: src2,
+    player: 'Ansu Fati'
+  },
+  {
+    src: src3,
+    player: 'Alphonso Davies'
+  },
+  {
+    src: src4,
+    player: 'Alphonso Davies'
+  },
+  {
+    src: src5,
+    player: 'Christian Pulisic'
+  },
+  {
+    src: src6,
+    player: 'Christian Pulisic'
+  },
+  {
+    src: src7,
+    player: 'Erling Haaland'
+  },
+  {
+    src: src8,
+    player: 'Erling Haaland'
+  },
+  {
+    src: src9,
+    player: 'Erling Haaland'
+  },
+  {
+    src: src10,
+    player: 'Erling Haaland'
+  },
+  {
+    src: src11,
+    player: 'Erling Haaland'
+  },
+  {
+    src: src12,
+    player: 'Erling Haaland'
+  },
+
+];
+
+export { images } 
